6a0bd3de54871d6febe8e9f3ee1373d8951ea902
Roel Stappers <Roel.stappers@gmail.com>
2020-05-06 13:44:23 +0200

    Clean up

--- src/HarmonieNamelist.jl
+++ src/HarmonieNamelist.jl
@@ -2,19 +2,12 @@ module HarmonieNamelist
 
 export schema2namelist
 
-
-const moduledir=@__DIR__ 
-
-const NAMELIST_DIR = "$moduledir/namelists/"
-const SCHEMA_DIR="$moduledir/jsonschema"
-
 tofortran(val::Bool) =  val ? ".TRUE."  :  ".FALSE."
 tofortran(val::Number) = val
 tofortran(val::AbstractString) = "'$val'"
-
+tofortran(val::Nothing) = ""
 
 function schema2namelist(io::IO, d::AbstractDict)
-    out = String[]
     println(io, "# $(d["description"])")
     for link in d["links"]
         println(io,"# $(link["rel"])")
@@ -22,15 +15,11 @@ function schema2namelist(io::IO, d::AbstractDict)
         println(io,"")
     end
     println(io,"&$(d["title"])   ")
-    prop= d["properties"]
-    descriptions = ["# $(get(val,"description",""))"  for (key,val) in prop]
-    defaults     = ["# $(get(val,"defaults",""))"     for (key,val) in prop]
-    keys         = ["$key ="                          for (key,val) in prop]
     for (key,property) in d["properties"]        
-        default = tofortran(get(property,"default","")) 
+        default     = tofortran(get(property,"default","")) 
         description = property["description"]
-        keydefault=rpad("$key=$default",25)
-        enum= join(get(property,"enum",""),", ")
+        keydefault  = rpad("$key=$default",25)
+        enum        = join(tofortran.(get(property,"enum",nothing)),", ")
         println(io, "  $keydefault  # $description [$enum]")
     end    
     println(io, "/")
